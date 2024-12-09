4b550ad1d4ff71a5f10530c491834d302cda6a3d
Dixser <daniom93@gmail.com>
2024-12-09 13:37:57 +0100

    part 5.2

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -11,64 +11,98 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+  const loginForm = () => {
+    const handleLogin = async (event) => {
+      event.preventDefault()
 
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      })
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      try {
+        const user = await loginService.login({
+          username,
+          password,
+        })
+
+        window.localStorage.setItem(
+          'loggedBlogappUser', JSON.stringify(user)
+        ) 
+        blogService.setToken(user.token)
+
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      } catch (exception) {
+        setErrorMessage('Wrong credentials')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
     }
-  }
-  if (user === null) {
     return (
       <>
-        <h1>{errorMessage}</h1>
         <form onSubmit={handleLogin}>
           <div>
             username
             <input
-              type="text"
+              type='text'
               value={username}
-              name="Username"
+              name='Username'
+              autoComplete='username'
               onChange={({ target }) => setUsername(target.value)}
             />
           </div>
           <div>
             password
             <input
-              type="password"
+              type='password'
               value={password}
-              name="Password"
+              name='Password'
+              autoComplete='current-password'
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type="submit">login</button>
+          <button type='submit'>login</button>
         </form>
       </>
     )
   }
+const handleLogout = () => {
+  setUser(null)
+  window.localStorage.removeItem('loggedBlogappUser')
+}
+  const blogForm = () => {
+    return (
+      <>
+        <div>
+          <h2>blogs</h2>
+          <p>
+            <strong>{user.username}</strong> logged in{' '}
+            <button type='button' onClick={handleLogout}>
+              logout
+            </button>
+          </p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>{errorMessage}</h1>
-      <div>
-        <h2>blogs</h2>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
-      </div>
+
+      {user === null ? loginForm() : blogForm()}
     </>
   )
 }
--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -6,4 +6,24 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-export default { getAll }
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${ baseUrl }/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update, setToken }
